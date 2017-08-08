30b2f71bd0068257dae3debd25fdb4dbd950c90c
currymich <currymich@gmail.com>
2017-08-08 11:13:09 -0700

    attempting to fix sendfile

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(logger('dev'));
 
 app.get('/', function(req, res){
-  res.sendFile('public/index.html');
+  res.sendfile('public/index.html');
 });
 
 app.post('/appointment', function(req,res) {
