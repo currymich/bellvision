2a7c34b2898d2ffb1411c29399bacbe133cb93f3
currymich <currymich@gmail.com>
2017-08-08 13:35:07 -0700

    Updating email formatting

--- server.js
+++ server.js
@@ -42,13 +42,41 @@ app.post('/appointment', function(req,res) {
       },
       content: [
         {
-          type: 'text/plain',
-          value: `Prefered Appointment Location: ${req.body.office}\n
-          Patient Name: ${req.body.name}\n
-          Patient DOB: ${req.body.dob}\n
-          Patient Email: ${req.body.email}\n
-          Patient Phone: ${req.body.phone}\n
-          Insurance: ${req.body.insurance}`
+          type: 'text/html',
+          value: `<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="Content-Style-Type" content="text/css">
+  <style type="text/css">
+    p.p1 {margin: 0.0px 0.0px 0.0px 0.0px; line-height: 43.0px; font: 24.0px Arial; color: #555555; -webkit-text-stroke: #555555}
+    p.p2 {margin: 0.0px 0.0px 0.0px 0.0px; line-height: 21.0px; font: 18.0px Arial; color: #555555; -webkit-text-stroke: #555555}
+    span.s1 {font-kerning: none; background-color: #ffffff}
+    span.s2 {font: 12.0px Arial; font-kerning: none}
+    table.t1 {border-collapse: collapse}
+    td.td1 {width: 500.0px; min-width: 320.0px; max-width: 500.0px}
+  </style>
+</head>
+<body>
+<table cellspacing="0" cellpadding="0" class="t1">
+  <tbody>
+    <tr>
+      <td valign="top" class="td1">
+        <p class="p1"><span class="s1"><b>﻿New Patient Application</b></span><span class="s2"><br><br>
+</span></p>
+        <p class="p2"><span class="s1">Prefered Office Location: ${req.body.office}</span></p>
+        <p class="p2"><span class="s1">Patient Name: ${req.body.name}</span></p>
+        <p class="p2"><span class="s1">Patient Phone Number: ${req.body.phone}</span></p>
+        <p class="p2"><span class="s1">Patient Email: ${req.body.email}</span></p>
+        <p class="p2"><span class="s1">Patient Date of Birth: ${req.body.dob}</span></p>
+        <p class="p2"><span class="s1">Patient Insurance Provider: ${req.body.insurance}</span></p>
+      </td>
+    </tr>
+  </tbody>
+</table>
+</body>
+</html>
+`
         }
       ]
     }
