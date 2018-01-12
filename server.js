af2d7f332848c1b75a0fcea99373139e638ceb1e
currymich <currymich@gmail.com>
2018-01-11 17:32:36 -0800

    more typos...

--- server.js
+++ server.js
@@ -63,9 +63,7 @@ app.post('/appointment', function(req,res) {
             </table>
             </body>
             </html>
-            `
-        }
-      ]
+            `      
     }
   });
   sg.send(msg)
