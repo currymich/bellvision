908ba131e32ce43c1f741e36f287526c7e7b9256
currymich <currymich@gmail.com>
2018-01-11 16:17:18 -0800

    updating to vision files

--- public/js/script.js
+++ public/js/script.js
@@ -1,48 +1,9 @@
-window.onload = function () {
-var menu = document.getElementById('menu'),
-    WINDOW_CHANGE_EVENT = ('onorientationchange' in window) ? 'orientationchange':'resize';
+$(document).on("scroll", () => {
 
-function toggleHorizontal() {
-    [].forEach.call(
-        document.getElementById('menu').querySelectorAll('.custom-can-transform'),
-        function(el){
-            el.classList.toggle('pure-menu-horizontal');
-        }
-    );
-};
+	if($(document).scrollTop()>100) {
+		$(".header").addClass("small");
+	} else {
+		$(".header").removeClass("small");
+	}
 
-function toggleMenu() {
-    // set timeout so that the panel has a chance to roll up
-    // before the menu switches states
-    if (menu.classList.contains('open')) {
-        setTimeout(toggleHorizontal, 500);
-    }
-    else {
-        toggleHorizontal();
-    }
-    menu.classList.toggle('open');
-    document.getElementById('toggle').classList.toggle('x');
-};
-
-function closeMenu() {
-    if (menu.classList.contains('open')) {
-        toggleMenu();
-    }
-}
-
-document.getElementById('toggle').addEventListener('click', function (e) {
-    toggleMenu();
-    e.preventDefault();
 });
-
-var submit = document.getElementById("submit")
-var form = document.getElementById("appointment")
-
-form.addEventListener("submit", function(e){
-    if(form.checkValidity() == true){
-      setTimeout(function(){form.innerHTML = 'Submitting appointment information, we will contact you shortly...';}, 250);
-    }
-});
-
-window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu);
-}
